c050c1da5e0071ba5c33895e684910af7ca25e1a
Jason Frantz <jason.frantz@hotmail.com>
2022-06-19 11:47:38 -0600

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Synthboard.Models;
+
+namespace Synthboard.Controllers;
+
+public class HomeController : Controller
+{
+
+    [HttpGet]
+    [Route("/")]
+    public IActionResult SynthboardSkeleton()
+    {
+        return View("SynthboardSkeleton");
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
