2f0177de86aaec7fe592952e114272aad1bba041
Jason Frantz <jason.frantz@hotmail.com>
2022-06-22 21:36:08 -0600

    LogReg Style and JS

---
+++ wwwroot/js/logregscript.js
@@ -0,0 +1,13 @@
+function registerBtn(elem) {
+    const divGrab = document.querySelector(".regdiv");
+
+    divGrab.style.transform = "scaleY(1)";
+    elem.style.color = "black";
+}
+
+function loginBtn(elem) {
+    const divGrab = document.querySelector(".logdiv");
+
+    divGrab.style.transform = "scaleY(1)";
+    elem.style.color = "black";
+}
