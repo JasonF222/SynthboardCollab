77e4940d7aec8fe3d5bc26db87f5192f0ea3c8f9
Stephen Nagasawa <stephen.nagasawa@me.com>
2022-06-23 12:13:29 -0600

    updated model display names

--- Models/LogUser.cs
+++ Models/LogUser.cs
@@ -7,10 +7,12 @@ public class LogUser
 {
     [Required(ErrorMessage = "is required.")]
     [EmailAddress(ErrorMessage = "must be a valid email address")]
+    [Display(Name = "Email")]
     public string LogEmail {get; set;}
     
     [Required(ErrorMessage = "is required.")]
     [DataType(DataType.Password)]
+    [Display(Name = "Password")]
     public string LogPass {get; set;}
 
 }
